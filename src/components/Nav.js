2e486ee33fcc9bcfa125b8404f4c26dc3eb9baa2
Jordan J <jordanr2m@gmail.com>
2024-08-21 14:32:41 -0700

    Finished Header and Nav components.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,29 @@
-
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import DataContext from '../context/DataContext.js';
 
 const Nav = () => {
+  const { search, setSearch } = useContext(DataContext);
+
   return (
     <nav className='Nav'>
-      <h1>Nav</h1>
+      {/* Form for nav search bar */}
+      <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor='search'>Search notes</label>
+        <input
+          id='search'
+          type='text'
+          placeholder='Search notes'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      {/* Nav Links */}
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="post">New Post</Link></li>
+        <li><Link to="about">Settings</Link></li>
+      </ul>
     </nav>
   )
 }
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -4,10 +4,11 @@ const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
     const [title, setTitle] = useState('Notebook App');
+    const [search, setSearch] = useState('');
 
   return (
     <DataContext.Provider value={{
-        title, setTitle
+        title, setTitle, search, setSearch
     }}>
         {children}
     </DataContext.Provider>
