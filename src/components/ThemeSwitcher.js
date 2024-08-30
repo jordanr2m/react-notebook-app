30bbfbdd03242624316e7b1ea91aac6ab7e71b6e
Jordan J <jordanr2m@gmail.com>
2024-08-30 11:33:30 -0700

    Added styles for theme settings buttons.

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 // Scoped styles
 import styles from './ThemeSwitcher.module.css';
+import { AiOutlineClose } from "react-icons/ai";
 
 const ThemeSwitcher = () => {
-  return (
-    <aside className={styles.wrapper}>
-      ThemeSwitcher
-    </aside>
-  )
+    const [isColorPicking, setIsColorPicking] = useState(false);
+
+    return (
+        <aside className={styles.wrapper}>
+            {isColorPicking ?
+                (
+                    <>
+                        <button></button>
+                        <input type='range' />
+                    </>
+                )
+                : (
+                    <div className={styles.btns}>
+                        <button className='theme-btn'>Dark Mode</button>
+                        <button className='theme-btn'>Choose Theme Color</button>
+                    </div>
+                )
+            }
+        </aside>
+    )
 }
 
 export default ThemeSwitcher
