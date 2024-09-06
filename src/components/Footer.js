68901c5a53a597eb1b0433087dc411aad2a4ec55
Jordan J <jordanr2m@gmail.com>
2024-09-06 15:45:08 -0700

    Updated Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import { useContext } from 'react';
 import DataContext from '../context/DataContext.js';
+import { format } from 'date-fns';
 
 const Footer = () => {
   const { notes } = useContext(DataContext);
+  const datetime = format(new Date(), 'MMMM dd, yyyy');
 
   return (
     <footer className="Footer">
-      <p>{notes.length} {notes.length === 1 ? "note" : "notes"}</p>
+      <p>{datetime}</p>
+      <p>{notes.length} {notes.length === 1 ? "note total" : "notes total"}</p>
     </footer>
   )
 }
